af6cfe2884a762422b2993270d5cb4a4556a0e22
Bathina Keerthi Priya <kpriyabathina@gmail.com>
2026-07-14 12:40:29 +0530

    Create dag_orchestrate_2.py

---
+++ dags/dag_orchestrate_2.py
@@ -0,0 +1,37 @@
+from airflow.sdk import dag, task
+import os
+
+@dag(
+    dag_id="second_orchestrator_dag",
+)
+def second_orchestrator_dag():
+
+    @task.python
+    def first_task():
+        print("This is the first task")
+
+    @task.python
+    def second_task():
+        print("This is the second task")
+
+    @task.python
+    def third_task():
+    #Ensure the directory exists
+        os.makedirs(os.path.dirnme("/opt/airflow/logs/data"), exist_ok=True)
+   
+        # Simulate data processing by writing to a file
+        with open("/opt/airflow/logs/dataoutput_2.txt", 'w') as f:
+            f.write(f"Data processed successfully")
+        
+
+    # Creating task instances
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+    # Defining task dependencies
+    first >> second >> third
+
+
+# Instantiating the DAG
+second_orchestrator_dag()
