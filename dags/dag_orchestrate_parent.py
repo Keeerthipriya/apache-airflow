9dc6cc20e46a2ef2cfbc7bd8956b8bed0ef1513e
Bathina Keerthi Priya <kpriyabathina@gmail.com>
2026-07-14 12:41:16 +0530

    Create dag_orchestrate_parent.py

---
+++ dags/dag_orchestrate_parent.py
@@ -0,0 +1,27 @@
+from dag_orchestrate_1 import first_orchestrator_dag
+from dag_orchestrate_2 import second_orchestrator_dag
+from airflow.sdk import dag,task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+
+@dag
+def dag_orchestrate_parent():
+     
+    #creating operators
+    trigger_first_dag=TriggerDagRunOperator(
+        task_id="trigger_first_archestrator_dag",
+        trigger_dag_id="first_orchestrator_dag",
+        wait_for_completion=True # Optional (This is slow because it will not run your task asynchronously): waits for the triggered DAG to complete
+    )
+
+    trigger_second_dag=TriggerDagRunOperator(
+        task_id="trigger_second_archestrator_dag",
+        trigger_dag_id="second_orchestrator_dag",
+        wait_for_completion=True # Optional (This is slow because it will not run your task asynchronously): waits for the triggered DAG to complete
+    )
+
+    
+    trigger_first_dag >> trigger_second_dag
+
+#Instantiating the DAG
+dag_orchestrate_parent()
